547dcd0501f7d936bf6e30e0713ea26347d8fb25
Gitskjha <shivam.webhopers@gmail.com>
2024-04-13 17:57:13 +0530

    fix half-screen problem

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,22 @@ import Navbar from "./Navbar";
 export default function Home() {
   return (
     //intro section
-    <div className="h-1/2 lg:h-screen bg-gradient-to-t from-indigo-200 relative overflow-hidden">
+    <div className="custom-h-1-2 lg:h-screen bg-gradient-to-t from-indigo-200 relative overflow-hidden">
       {/* navigation bar */}
       <Navbar />
       {/* full image */}
       <img
-        className="absolute bottom-0 right-0 h-5/6 "
+        className="absolute bottom-0 lg:left-0 mx-auto right-0 h-5/6 object-cover "
         src={"/assets/man.png"}
         alt="my-image"
       />
       {/* circle*/}
-      <div className="hidden lg:block -bottom-1/4 absolute w-big h-big bg-indigo-900 right-0 left-0 mx-auto rounded-full -z-10 "></div>
-      {/* //intro content */}
-      <div></div>
+      <div className="hidden lg:block  absolute -bottom-1/4 w-big h-big bg-indigo-900 right-0 left-0 mx-auto rounded-full -z-10 "></div>
+      {/* animated text */}
+      <div className="absolute top-1/3">
+        <span className="text-gray-600">Full Stack</span>
+        <div className="text-red-500">Developer</div>
+      </div>
     </div>
   );
 }
